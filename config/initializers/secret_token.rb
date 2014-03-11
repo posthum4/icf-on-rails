0938fa2f1945e729066ac235d3776255c69b4368
Roland Siebelink <git@rolandsiebelink.com>
2014-03-10 18:19:55 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IcfOnRails::Application.config.secret_key_base = 'db23206c379f5a2bcb6642d991aef8cd09c80f451c65ad19a5a8effe0c46450861889762571f269a1758aee93ea7f119aea1ffbbf93c498e018ef504de70417b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IcfOnRails::Application.config.session_store :cookie_store, key: '_icf-on-rails_session'
