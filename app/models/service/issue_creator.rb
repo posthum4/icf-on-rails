6e9fd601eaffc96cc53c1916d1d26acf82e93512
Roland Siebelink <git@rolandsiebelink.com>
2014-05-18 14:33:55 -0700

    Got parent import working, though testing is not easy, too little live data

--- app/models/service/issue_creator.rb
+++ app/models/service/issue_creator.rb
@@ -3,15 +3,27 @@ module Service
 
     attr_accessor :sfdcid, :campaign_order, :jira, :fields, :description
 
-    def initialize(campaign_order)
+    def initialize(campaign_order,parent=false)
       @co       = campaign_order
       @sfdcid   = @co.sfdcid
       @fields   = Value::Field.new
       @jira_key = @co.jira_key
       @jira     = Jira::Issue.find_by_key(@jira_key) unless @jira_key.nil?
+      @parent   = parent
     end
 
-    def find_or_create_jira_by_campaign_order
+    def find_jira_by_campaign_order
+      fail CampaignOrderMissing, @co if ( !@co || @co.nil? )
+      @jira = Jira::Issue.find_by_campaign_order(@co) rescue nil
+      unless @jira.nil?
+        @jira_key = @jira.key
+        @co.jira_key = @jira_key
+        @co.save
+      end
+      @jira
+    end
+
+    def create_jira_by_campaign_order
       fail CampaignOrderMissing, @co if ( !@co || @co.nil? )
       @jira = Jira::Issue.find_or_create_by_campaign_order(@co, self.subject)
       @jira_key = @jira.key
@@ -21,12 +33,13 @@ module Service
     end
 
     def import_from_campaign_order
-      @jira = find_or_create_jira_by_campaign_order if @jira.nil?
-      # second test: if still no JIRA then fail
-      fail Exceptions::JiraUnknownIssueNumberError, @sfdcid.to_s if @jira.nil?
-      if !@jira.key.nil? and @jira.key.starts_with?('ICF-')
-        fail Warnings::JiraAlreadyExisted_NotOverwritten, @jira.key
+      @jira = find_jira_by_campaign_order if @jira.nil?
+      if !@jira.nil? and !@jira.key.nil? and @jira.key.starts_with?('ICF-')
+        warn Warnings::JiraAlreadyExisted_NotOverwritten, "https://rocketfuel.jira.com/browse/#{@jira.key}"
       else
+        @jira = create_jira_by_campaign_order
+        # second test: if still no JIRA then fail
+        fail Exceptions::JiraUnknownIssueNumberError, @sfdcid.to_s if @jira.nil?
         import_tables
         Rails.logger.info 'Imported tables'
         import_attachments
@@ -60,13 +73,17 @@ module Service
     end
 
     def subject
-      case @co.opp_type_new
-      when 'Media: New Business', 'Enterprise: New Business'
-        mymarker = 'Launch'
+      if @parent
+        result = "Original #{@co.name} [use as parent JIRA for IO changes]"
       else
-        mymarker = 'Change'
+        case @co.opp_type_new
+        when 'Media: New Business', 'Enterprise: New Business'
+          mymarker = 'Launch'
+        else
+          mymarker = 'Change'
+        end
+        result = "#{mymarker} #{@co.name} due #{@co.campaign_start_date}"
       end
-      "#{mymarker} #{@co.name} due #{@co.campaign_start_date}"
     end
 
   end
