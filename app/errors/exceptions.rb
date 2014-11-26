bb156c3b88b007a843631765c28a10f36453781b
Roland Siebelink <git@rolandsiebelink.com>
2014-11-25 18:29:21 -0800

    v2.4.4c - catching more errors

--- app/errors/exceptions.rb
+++ app/errors/exceptions.rb
@@ -4,6 +4,7 @@ module Exceptions
   class JiraAlreadyExistsError < StandardError ; end
   class OpportunityAlreadyImportedLocallyAndNotForcedError < StandardError ; end
   class InvalidSalesForceOpportunityIDNotStartingWith0068 < StandardError ; end
+  class InvalidSalesForceOpportunityID < StandardError ; end
   class SalesForceClientError < StandardError ; end
   class MissingJiraKeyError < StandardError ; end
   class MissingEmailIDError < StandardError ; end
